56f318746096d2f5771b89dbe7cd25b1c55497a0
BragomRS <brandongomezrocha@gmail.com>
2026-06-28 22:43:59 -0400

    Local Storage agregado.

--- script.js
+++ script.js
@@ -1,40 +1,49 @@
+const seleccionados = JSON.parse(localStorage.getItem("gustambitos")) || [];
+
 const gustambitos = [
     {
-        nombre: "Gustambito 1",
-        imagen: "assets/images/ejemplo.png",
-        categoria: 1
-    },
+    id: 1,
+    nombre: "Gustambito 1",
+    imagen: "assets/images/ejemplo.png",
+    categoria: 1
+},
     {
-        nombre: "Gustambito 2",
-        imagen: "assets/images/ejemplo.png",
-        categoria: 1
-    },
+    id: 2,
+    nombre: "Gustambito 2",
+    imagen: "assets/images/ejemplo.png",
+    categoria: 1
+},
+{
+    id: 3,
+    nombre: "Gustambito 3",
+    imagen: "assets/images/ejemplo.png",
+    categoria: 2
+},
+{
+    id: 4,
+    nombre: "Gustambito 4",
+    imagen: "assets/images/ejemplo.png",
+    categoria: 2
+},
     {
-        nombre: "Gustambito 3",
-        imagen: "assets/images/ejemplo.png",
-        categoria: 2
-    },
-    {
-        nombre: "Gustambito 4",
-        imagen: "assets/images/ejemplo.png",
-        categoria: 2
-    },
-    {
+        id: 5,
         nombre: "Gustambito 5",
         imagen: "assets/images/ejemplo.png",
         categoria: 3
     },
     {
+        id: 6,
         nombre: "Gustambito 6",
         imagen: "assets/images/ejemplo.png",
         categoria: 3
     },
     {
+    id: 7,
     nombre: "Gustambito 7",
     imagen: "assets/images/ejemplo.png",
     categoria: 4
 },
-{
+{   id: 8,
     nombre: "Gustambito 8",
     imagen: "assets/images/ejemplo.png",
     categoria: 5
@@ -49,7 +58,7 @@ gustambitos.forEach(gustambito => {
 
     // Creamos la tarjeta
     const tarjeta = document.createElement("div");
-
+    
     tarjeta.className = "tarjeta";
 
     tarjeta.innerHTML = `
@@ -57,10 +66,35 @@ gustambitos.forEach(gustambito => {
         <p>${gustambito.nombre}</p>
     `;
 
-    // Activar/desactivar
-    tarjeta.addEventListener("click", () => {
-        tarjeta.classList.toggle("activa");
-    });
+    // Selector del local 
+    if (seleccionados.includes(gustambito.id)) {
+        tarjeta.classList.add("activa");
+    }
+
+    // Activar/desactivar y guardar en localStorage
+tarjeta.addEventListener("click", () => {
+
+    tarjeta.classList.toggle("activa");
+
+    if (tarjeta.classList.contains("activa")) {
+
+        if (!seleccionados.includes(gustambito.id)) {
+            seleccionados.push(gustambito.id);
+        }
+
+    } else {
+
+        const indice = seleccionados.indexOf(gustambito.id);
+
+        if (indice !== -1) {
+            seleccionados.splice(indice, 1);
+        }
+
+    }
+
+    localStorage.setItem("gustambitos", JSON.stringify(seleccionados));
+
+});
 
     // Agregar al contenedor correcto
     contenedor.appendChild(tarjeta);
