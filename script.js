1374dcfc7b12dd5908064e709e09771797d36359
BragomRS <brandongomezrocha@gmail.com>
2026-06-28 23:15:19 -0400

    imganes reales
    se agregaron las imagenes y la lista para la categoria 1 normal

--- script.js
+++ script.js
@@ -1,18 +1,115 @@
 const seleccionados = JSON.parse(localStorage.getItem("gustambitos")) || [];
 
 const gustambitos = [
-    {
+   {
     id: 1,
-    nombre: "Gustambito 1",
-    imagen: "assets/images/ejemplo.png",
+    nombre: "Espíritu de Agua",
+    imagen: "assets/images/base/1.png",
     categoria: 1
 },
-    {
+{
     id: 2,
-    nombre: "Gustambito 2",
-    imagen: "assets/images/ejemplo.png",
+    nombre: "Espíritu de Tierra",
+    imagen: "assets/images/base/2.png",
     categoria: 1
 },
+{
+    id: 3,
+    nombre: "Espíritu de Fuego",
+    imagen: "assets/images/base/3.png",
+    categoria: 1
+},
+{
+    id: 4,
+    nombre: "Espíritu de Aire",
+    imagen: "assets/images/base/4.png",
+    categoria: 1
+},
+{
+    id: 5,
+    nombre: "Espíritu Pez",
+    imagen: "assets/images/base/5.png",
+    categoria: 1
+},
+{
+    id: 6,
+    nombre: "Espíritu Pato",
+    imagen: "assets/images/base/6.png",
+    categoria: 1
+},
+{
+    id: 7,
+    nombre: "Espíritu Demonio",
+    imagen: "assets/images/base/7.png",
+    categoria: 1
+},
+{
+    id: 8,
+    nombre: "Espíritu Fantasma",
+    imagen: "assets/images/base/8.png",
+    categoria: 1
+},
+{
+    id: 9,
+    nombre: "Espíritu Rey",
+    imagen: "assets/images/base/9.png",
+    categoria: 1
+},
+{
+    id: 10,
+    nombre: "Espíritu Atacante",
+    imagen: "assets/images/base/10.png",
+    categoria: 1
+},
+{
+    id: 11,
+    nombre: "Espíritu del Aura",
+    imagen: "assets/images/base/11.png",
+    categoria: 1
+},
+{
+    id: 12,
+    nombre: "Espíritu Punk",
+    imagen: "assets/images/base/12.png",
+    categoria: 1
+},
+{
+    id: 13,
+    nombre: "Espíritu de los Sueños",
+    imagen: "assets/images/base/13.png",
+    categoria: 1
+},
+{
+    id: 14,
+    nombre: "Espíritu Jefe",
+    imagen: "assets/images/base/14.png",
+    categoria: 1
+},
+{
+    id: 15,
+    nombre: "Espíritu Siete",
+    imagen: "assets/images/base/15.png",
+    categoria: 1
+},
+{
+    id: 16,
+    nombre: "Espíritu del Punto Cero",
+    imagen: "assets/images/base/16.png",
+    categoria: 1
+},
+{
+    id: 17,
+    nombre: "Espíritu Siniestro",
+    imagen: "assets/images/base/17.png",
+    categoria: 1
+},
+{
+    id: 18,
+    nombre: "Maní Quemado",
+    imagen: "assets/images/base/18.png",
+    categoria: 1
+},
+
 {
     id: 3,
     nombre: "Gustambito 3",
